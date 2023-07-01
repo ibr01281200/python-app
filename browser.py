535c5f10171a1e7f9340eb581343f8ae7e36e7eb
ibr01281200 <128221004+ibr01281200@users.noreply.github.com>
2023-07-01 09:07:35 +0300

    Add files via upload

---
+++ browser.py
@@ -0,0 +1,35 @@
+import webbrowser
+from tkinter import *
+
+root =Tk()
+root.geometry('400x200')
+root.title('ibr20100')
+root.resizable(False,False)
+root.config(background='#4C4646')
+
+#====== انشاء ادالة ======
+def search():
+    t=e.get()
+    webbrowser.open(t)
+
+
+
+#=========
+lb1 = Label(root,text='Web Browser',fg='white',bg='black',font=('Courier',12,'bold'))
+lb1.pack(fill=X)
+
+e =Entry(root, width=60,fg='red',font='bold')
+e.pack()
+
+btn=Button(root,text='Search',activebackground='black',activeforeground='white',fg='green' ,font=('Courier',14,'bold'),command=search)
+btn.pack()
+
+
+
+
+
+
+
+root.mainloop()
+
+
